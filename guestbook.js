f8d9e51ed2000dcefe5b74fab5e466c9f6979084
Sefa Ozdogan <sefaozdogan@gmail.com>
2022-02-15 08:35:47 +0300

    Insert apiUrl

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://7acf4dd4.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
